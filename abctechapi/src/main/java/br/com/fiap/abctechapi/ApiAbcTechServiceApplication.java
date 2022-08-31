9a8bbd707519277da435c29ce7f5d08b628e6135
ELSYS\rodrigo.caraca <rodrigo.caraca@elsys.com.br>
2022-08-30 23:52:13 -0300

    Arquivos iniciais

---
+++ abctechapi/src/main/java/br/com/fiap/abctechapi/ApiAbcTechServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAbcTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAbcTechServiceApplication.class, args);
+	}
+
+}
