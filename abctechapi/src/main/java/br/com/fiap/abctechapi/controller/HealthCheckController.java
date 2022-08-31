8008d5cfb12aae4f3d5ab27ad844d69ce7d26fa5
ELSYS\rodrigo.caraca <rodrigo.caraca@elsys.com.br>
2022-08-31 00:10:23 -0300

    Alterado endpoint de version

--- abctechapi/src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ abctechapi/src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
 
-    @GetMapping()
-    public ResponseEntity<Object> status() {
-        return ResponseEntity.ok().build();
+    @GetMapping("version")
+    public ResponseEntity<String> status() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yaml");
+
+        properties.load(inputStream);
+
+        return ResponseEntity.ok("Name: " + properties.getProperty("build.name") + " - " + "Version: " + properties.getProperty("build.version"));
     }
 }
