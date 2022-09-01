b285fe8f86fef644ac2be5bfced5190b37744854
ELSYS\rodrigo.caraca <rodrigo.caraca@elsys.com.br>
2022-09-01 19:11:21 -0300

    Implementações e testes (com 1 erro - forçando erro na pipeline da AWS)

---
+++ src/test/java/br/com/fiap/abctechapi/service/AssistanceServiceTests.java
@@ -0,0 +1,57 @@
+package br.com.fiap.abctechapi.service;
+
+import br.com.fiap.abctechapi.model.Assistance;
+import br.com.fiap.abctechapi.repository.AssistanceRepository;
+import br.com.fiap.abctechapi.service.impl.AssistanceServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AssistanceServiceTests {
+
+    @Mock
+    private AssistanceRepository assistanceRepository;
+    private AssistanceService assistanceService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        assistanceService = new AssistanceServiceImpl(assistanceRepository);
+    }
+
+    @DisplayName("Listando assistências disponíveis :: Sucesso")
+    @Test
+    public void list_success() {
+        Assistance assistance1 = new Assistance(1L, "Mock Assistance 1", "Description 1");
+        Assistance assistance2 = new Assistance(2L, "Mock Assistance 2", "Description 2");
+        Assistance assistance3 = new Assistance(3L, "Mock Assistance 3", "Description 3");
+
+        when(assistanceRepository.findAll()).thenReturn(List.of(assistance1, assistance2, assistance3));
+
+        List<Assistance> values = assistanceService.getAssistanceList();
+
+        Assertions.assertEquals(4, values.size());
+        Assertions.assertSame(values.get(0), assistance1);
+        Assertions.assertSame(values.get(1), assistance2);
+        Assertions.assertSame(values.get(2), assistance3);
+    }
+
+    @DisplayName("Listando assistências indisponíveis :: Erro")
+    @Test
+    public void list_error() {
+        when(assistanceRepository.findAll()).thenReturn(List.of());
+
+        List<Assistance> values = assistanceService.getAssistanceList();
+
+        Assertions.assertEquals(0, values.size());
+    }
+}
